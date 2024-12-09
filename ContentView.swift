01ae70fee4b05f904dcbf9dc60ffdd3af173190e
johannasoldis1 <hystrom@ug.kth.se>
2024-12-09 10:09:50 +0100

    Update ContentView.swift
    With max rms

--- ContentView.swift
+++ ContentView.swift
@@ -8,183 +8,151 @@ struct ContentView: View {
     @State var file_content: TextFile = TextFile(initialText: "")
 
     var body: some View {
-        VStack(spacing: 1) { // Adjust spacing between sections
+        GeometryReader { geometry in
+            VStack(spacing: 10) {
 
-            // Raw EMG Graph
-            VStack {
-                Text("Raw EMG Data")
-                    .font(.headline)
-                    .padding(.top, 10)
-
-                Path { path in
-                    let height = UIScreen.main.bounds.height / 6
-                    let width = UIScreen.main.bounds.width
-
-                    guard graph.values.count > 1 else { return }
-
-                    let firstSample = { () -> Int in
-                        if graph.values.count > 50 {
-                            return graph.values.count - 50
-                        } else {
-                            return 0
-                        }
-                    }
-                    let cutGraph = graph.values[firstSample()..<graph.values.count]
-                    guard !cutGraph.isEmpty else { return }
-                    let midY = height / 2
-
-                    let startX = CGFloat(0)
-                    let startY = midY - (height / 2 * CGFloat(cutGraph.first ?? 0))
-                    path.move(to: CGPoint(x: startX, y: startY))
-
-                    cutGraph.enumerated().forEach { index, item in
-                        path.addLine(
-                            to: CGPoint(
-                                x: width * CGFloat(index) / (CGFloat(cutGraph.count) - 1.0),
-                                y: midY - (height / 2 * CGFloat(item))
-                            )
-                        )
-                    }
-                }
-                .stroke(Color.blue, lineWidth: 1.5)
-                .frame(height: 150)
-            }
-
-            // RMS Graph
-            VStack {
-                Text("RMS Data")
-                    .font(.headline)
-                    .padding(.top, 10)
-
-                Path { path in
-                    let height = UIScreen.main.bounds.height / 8
-                    let width = UIScreen.main.bounds.width
-                    let history = BLE.rmsHistory
-
-                    guard !history.isEmpty else { return }
-                    let midY = height / 2
-
-                    let startX = CGFloat(0)
-                    let startY = midY - (height / 2 * CGFloat(history.first ?? 0))
-                    path.move(to: CGPoint(x: startX, y: startY))
-
-                    for (index, value) in history.enumerated() {
-                        let x = startX + CGFloat(index) * (width / CGFloat(history.count - 1))
-                        let y = midY - (height / 2 * CGFloat(value))
-                        path.addLine(to: CGPoint(x: x, y: y))
-                    }
-                }
-                .stroke(Color.red, lineWidth: 2.0)
-                .frame(height: 120)
-            }
-
-            // Current RMS Value
-            Text("Current RMS: \(BLE.currentRMS, specifier: "%.2f")")
-                .font(.headline)
-                .foregroundColor(.red)
-                .padding()
-
-            if !BLE.isConnected {
-                // Show connection options only if not connected
+                // Raw EMG Graph
                 VStack {
-                    Text("Connect to Sensor")
-                        .font(.title)
-                        .frame(maxWidth: .infinity, alignment: .center)
+                    Text("Raw EMG Data")
+                        .font(.headline)
 
-                    List(BLE.BLEPeripherals) { peripheral in
-                        HStack {
-                            Text(peripheral.name).onTapGesture {
-                                print(peripheral)
-                                BLE.connectSensor(p: peripheral)
-                            }
-                            Spacer()
-                            Text(String(peripheral.rssi))
+                    Path { path in
+                        let height = geometry.size.height / 8
+                        let width = geometry.size.width
+
+                        guard graph.values.count > 1 else { return }
+
+                        let firstSample = max(0, graph.values.count - 50)
+                        let cutGraph = graph.values[firstSample..<graph.values.count]
+                        let midY = height / 2
+
+                        guard !cutGraph.isEmpty else { return }
+
+                        path.move(to: CGPoint(x: 0, y: midY - height / 2 * CGFloat(cutGraph.first ?? 0)))
+
+                        for (index, value) in cutGraph.enumerated() {
+                            let x = width * CGFloat(index) / CGFloat(cutGraph.count - 1)
+                            let y = midY - height / 2 * CGFloat(value)
+                            path.addLine(to: CGPoint(x: x, y: y))
                         }
                     }
-                    .frame(height: 80)
+                    .stroke(Color.blue, lineWidth: 1.5)
+                    .frame(height: geometry.size.height / 8)
                 }
-            } else {
-                // Display a message when connected
-                Text("Connected to EMGBLE2!")
-                    .font(.headline)
+
+                // 1-Second RMS Graph
+                VStack {
+                    Text("1-Second RMS Data")
+                        .font(.headline)
+
+                    Path { path in
+                        let height = geometry.size.height / 12
+                        let width = geometry.size.width
+
+                        guard !BLE.max1SecRMSHistory.isEmpty else { return }
+                        let midY = height / 2
+
+                        path.move(to: CGPoint(x: 0, y: midY - height / 2 * CGFloat(BLE.max1SecRMSHistory.first ?? 0)))
+
+                        for (index, value) in BLE.max1SecRMSHistory.enumerated() {
+                            let x = CGFloat(index) * width / CGFloat(BLE.max1SecRMSHistory.count - 1)
+                            let y = midY - height / 2 * CGFloat(value)
+                            path.addLine(to: CGPoint(x: x, y: y))
+                        }
+                    }
+                    .stroke(Color.green, lineWidth: 2.0)
+                    .frame(height: geometry.size.height / 12)
+                }
+
+                // Current 1-Second RMS Value
+                Text("1-Second RMS: \(BLE.max1SecRMS, specifier: "%.2f")")
+                    .font(.system(size: 14))
                     .foregroundColor(.green)
-            }
 
-            // Status Display
-            Text("STATUS")
-                .font(.headline)
-            if BLE.BLEisOn {
-                Text("Bluetooth is switched on")
-                    .foregroundColor(.green)
-            } else {
-                Text("Bluetooth is NOT switched on")
-                    .foregroundColor(.red)
-            }
+                // Connect to Sensor Section
+                if !BLE.isConnected {
+                    VStack {
+                        Text("Connect to Sensor")
+                            .font(.headline)
 
-            // Buttons for Bluetooth scanning and recording
-            HStack {
-                VStack(spacing: 5) { // Vertically stacked scanning buttons
-                    Button(action: {
-                        BLE.startScanning()
-                    }) {
-                        Text("Start Scanning")
-                    }
-                    .disabled(BLE.isConnected) // Disable if connected
-
-                    Button(action: {
-                        BLE.stopScanning()
-                    }) {
-                        Text("Stop Scanning")
-                    }
-                    .disabled(!BLE.BLEisOn || BLE.isConnected) // Disable if Bluetooth is off or connected
-                }
-                .padding()
-
-                Spacer()
-
-                VStack(spacing: 10) { // Vertically stacked recording buttons
-                    Button(action: {
-                        DispatchQueue.global(qos: .background).async {
-                            graph.record()
-                            DispatchQueue.main.async {
-                                print("Recording started.")
+                        List(BLE.BLEPeripherals) { peripheral in
+                            HStack {
+                                Text(peripheral.name).onTapGesture {
+                                    BLE.connectSensor(p: peripheral)
+                                }
+                                Spacer()
+                                Text("\(peripheral.rssi)")
                             }
                         }
-                    }) {
-                        Text("Start Recording")
+                        .frame(height: geometry.size.height / 10)
                     }
+                } else {
+                    Text("Connected to EMGBLE2!")
+                        .font(.headline)
+                        .foregroundColor(.green)
+                }
 
-                    Button(action: {
-                        DispatchQueue.global(qos: .background).async {
-                            let fileContent = graph.stop_recording_and_save()
-                            DispatchQueue.main.async {
-                                file_content.text = fileContent
-                                print("Recording stopped and saved.")
-                            }
-                        }
-                    }) {
-                        Text("Stop Recording")
-                    }
-
-                    Button(action: {
-                        showingExporter = true
-                    }) {
-                        Text("Export last")
+                // Status Display
+                VStack {
+                    Text("STATUS")
+                        .font(.headline)
+                    if BLE.BLEisOn {
+                        Text("Bluetooth is switched on")
+                            .foregroundColor(.green)
+                    } else {
+                        Text("Bluetooth is NOT switched on")
+                            .foregroundColor(.red)
                     }
                 }
-                .padding()
+
+                // Buttons for Bluetooth scanning and recording
+                HStack {
+                    VStack(spacing: 5) {
+                        Button("Start Scanning") { BLE.startScanning() }
+                            .disabled(BLE.isConnected)
+
+                        Button("Stop Scanning") { BLE.stopScanning() }
+                            .disabled(!BLE.BLEisOn || BLE.isConnected)
+                    }
+                    .padding()
+
+                    Spacer()
+
+                    VStack(spacing: 5) {
+                        Button("Start Recording") {
+                            DispatchQueue.global(qos: .background).async {
+                                graph.record()
+                                DispatchQueue.main.async {
+                                    print("Recording started.")
+                                }
+                            }
+                        }
+
+                        Button("Stop Recording") {
+                            DispatchQueue.global(qos: .background).async {
+                                let fileContent = graph.stop_recording_and_save()
+                                DispatchQueue.main.async {
+                                    file_content.text = fileContent
+                                    print("Recording stopped and saved.")
+                                }
+                            }
+                        }
+
+                        Button("Export last") { showingExporter = true }
+                    }
+                    .padding()
+                }
             }
-            Spacer()
-        }
-        .fileExporter(isPresented: $showingExporter, document: file_content, contentType: .commaSeparatedText, defaultFilename: "emg-data") { result in
-            switch result {
-            case .success(let url):
-                print("Saved to \(url)")
-            case .failure(let error):
-                print(error.localizedDescription)
+            .padding(10)
+            .fileExporter(isPresented: $showingExporter, document: file_content, contentType: .commaSeparatedText, defaultFilename: "emg-data") { result in
+                switch result {
+                case .success(let url):
+                    print("Saved to \(url)")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
             }
         }
-        .padding(10)
     }
 }
 
